c470a8dbc565467afc1aac5e79edebfae1d7dd2b
xiomara-masmela <xiomara-masmela@gmail.com>
2021-09-10 20:51:36 +1000

    config db

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import os
 import psycopg2
 
 DB_URL = os.environ.get("DATABASE_URL", "dbname=feedback_app")
+SECRET_KEY = os.environ.get("SECRET_KEY", "pretend key for testing only")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 @app.route('/')
 def index():
