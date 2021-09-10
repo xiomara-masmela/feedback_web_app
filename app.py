c6c159d50b320375afb4661667837985f7a6decd
xiomara-masmela <xiomara-masmela@gmail.com>
2021-09-10 20:44:59 +1000

    db

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import os
+import psycopg2
+
+DB_URL = os.environ.get("DATABASE_URL", "dbname=feedback_app")
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    conn = psycopg2.connect(DB_URL)
+    cur = conn.cursor()
+    cur.execute('SELECT 1', []) # Query to check that the DB connected
+    conn.close()
+    return 'Hello, world Yang!'
 
 if __name__ == "__main__":
     app.run(debug=True)
