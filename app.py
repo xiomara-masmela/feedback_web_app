00930f3101f5ca59f775de419c89dd0456c9f6df
xiomara-masmela <xiomara-masmela@gmail.com>
2021-09-11 12:09:48 +1000

    Project templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 import os
 import psycopg2
 
@@ -8,7 +8,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "pretend key for testing only")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-
+# home
+@app.route('/home')
 @app.route('/')
 def index():
     conn = psycopg2.connect(DB_URL)
@@ -16,6 +17,47 @@ def index():
     cur.execute('SELECT 1', []) # Query to check that the DB connected
     conn.close()
     return render_template('index.html')
+
+# login
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# @app.route('/login_action', methods=['POST'])
+# def loginAction():
+#     #form
+#     username = request.form.get('username')
+#     password = request.form.get('password')
+    
+#     #db
+#     users = all_users()
+#     for user in users:  
+#         password_hash = user['password_hash']
+#         valid = bcrypt.checkpw(password.encode(), password_hash.encode())
+#         print(password_hash, valid, password)
+#         if user['email'] == username and valid == True:
+#             session['email'] = username
+#             session['user_id'] = user['id']
+#             session['name'] = user['name']
+#             return redirect('/')
+#         else:
+#             error_message = 'Email address or password is incorrect'
+#             return render_template('login.jinja', error_message = error_message)
+#     return " user not found"
+
+# signup
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+# logout
+
+# upload project
+@app.route('/upload-project')
+def uploadProject():
+    return render_template('upload-project.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
